d096742343c98e44f33138274f65da1e227ea6af
suon <cross-fire@list.ru>
2023-12-05 21:30:29 +0200

    Added multichat

--- main.py
+++ main.py
@@ -8,24 +8,50 @@ print(api_key)
 
 client = OpenAI(api_key=api_key)
 
+system_prompt = "You're an SEO specialist and know all the ways to promote websites in search engines."
 
-def get_response(system_prompt, user_prompt):
+system_role = {"role": "system", "content": system_prompt}
+
+temperature = 0
+
+
+def get_response(message_log):
     # Create a request to the ChatCompletion endpoint
     response = client.chat.completions.create(
-      model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo",
       # Assign the role and content for the message
-      messages=[{"role": "system", "content": system_prompt},
-                {"role": 'user', "content": user_prompt}], temperature=0)
+        messages=message_log)
     return response
 
 
 def main():
-    response = get_response("You are a chatbot that answers SEO questions.", "Who are you?")
-    print(response)
-    print(response.choices)
-    print(response.choices[0])
-    print(response.choices[0].message)
-    print(response.choices[0].message.content)
+    messages = [{'role': 'system', 'content': system_prompt}]
+
+    print(f"""Description of the bot role:
+    {system_prompt}""")
+    while True:
+        user_massage = input('Enter your message: ')
+
+        if user_massage == 'qqq':
+            break
+
+        messages.append({'role': 'user', 'content': user_massage})
+        response = get_response(messages)
+        assist_message = response.choices[0].message
+        messages.append(assist_message)
+        print(f"""Botty answer:
+        - {response.choices[0].message.content}
+        """)
+
+
+
+    # response = get_response("You are a chatbot that answers SEO questions.", "Who are you?")
+
+    # print(response)
+    # print(response.choices)
+    # print(response.choices[0])
+    # print(response.choices[0].message)
+    # print(response.choices[0].message.content)
 
 
 if __name__ == '__main__':
